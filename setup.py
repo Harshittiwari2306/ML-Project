cb0145e0a21c33020cab101a24af2b4bf3c6e74f
Harshit Tiwari <harshittiwari2306@gmail.com>
2026-05-22 23:24:44 +0530

    EDA & Model Training

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import find_packages,setup
 from typing import List
 
+HEPEN_E_DOT = '-e .'
+
 def get_requirements(file_path:str) -> list[str]: 
     '''This function will return the list of requirements'''
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n","") for req in requirements]
+
+    if HEPEN_E_DOT in requirements:
+        requirements.remove(HEPEN_E_DOT)
 
 setup(
     name='mlproject',
